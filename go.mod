f380a3abe36cdc4e1c217ded01989018c4e8de52
Cristian Maglie <c.maglie@arduino.cc>
2018-11-29 17:11:09 +0100

    inital import

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/arduino/serial-discovery
+
+require (
+	github.com/arduino/go-paths-helper v0.0.0-20180911135717-751652ddd9f0 // indirect
+	github.com/arduino/go-properties-orderedmap v0.0.0-20181003091528-89278049acd3
+	github.com/creack/goselect v0.0.0-20180501195510-58854f77ee8d // indirect
+	go.bug.st/serial.v1 v0.0.0-20180827123349-5f7892a7bb45
+	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
+)
---
+++ main.go
@@ -0,0 +1,130 @@
+/*
+ * This file is part of serial-discovery.
+ *
+ * Copyright 2018 ARDUINO SA (http://www.arduino.cc/)
+ *
+ * This software is released under the GNU General Public License version 3,
+ * which covers the main part of arduino-cli.
+ * The terms of this license can be found at:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * You can be released from the requirements of the above licenses by purchasing
+ * a commercial license. Buying such a license is mandatory if you want to modify or
+ * otherwise use the software for commercial activities involving the Arduino
+ * software without disclosing the source code of your own applications. To purchase
+ * a commercial license, send an email to license@arduino.cc.
+ */
+
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/arduino/go-properties-orderedmap"
+	"go.bug.st/serial.v1/enumerator"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		cmd, err := reader.ReadString('\n')
+		if err != nil {
+			outputError(err)
+			os.Exit(1)
+		}
+		cmd = strings.ToUpper(strings.TrimSpace(cmd))
+		switch cmd {
+		case "START":
+			outputMessage("start", "OK")
+		case "STOP":
+			outputMessage("stop", "OK")
+		case "LIST":
+			outputList()
+		case "QUIT":
+			outputMessage("quit", "OK")
+			os.Exit(0)
+		case "START_SYNC":
+			fallthrough
+		default:
+			outputError(fmt.Errorf("Command %s not supported", cmd))
+		}
+	}
+}
+
+type boardPortJSON struct {
+	Address             string          `json:"address"`
+	Label               string          `json:"label"`
+	Prefs               *properties.Map `json:"prefs"`
+	IdentificationPrefs *properties.Map `json:"identificationPrefs"`
+	Protocol            string          `json:"protocol"`
+	ProtocolLabel       string          `json:"protocolLabel"`
+}
+
+type listOutputJSON struct {
+	EventType string           `json:"eventType"`
+	Ports     []*boardPortJSON `json:"ports"`
+}
+
+func outputList() {
+	list, err := enumerator.GetDetailedPortsList()
+	if err != nil {
+		outputError(err)
+		return
+	}
+	portsJSON := []*boardPortJSON{}
+	for _, port := range list {
+		prefs := properties.NewMap()
+		identificationPrefs := properties.NewMap()
+		portJSON := &boardPortJSON{
+			Address:             port.Name,
+			Label:               port.Name,
+			Protocol:            "serial",
+			ProtocolLabel:       "Serial Port",
+			Prefs:               prefs,
+			IdentificationPrefs: identificationPrefs,
+		}
+		if port.IsUSB {
+			portJSON.ProtocolLabel = "USB Serial Port"
+			portJSON.Prefs.Set("vendorId", "0x"+port.VID)
+			portJSON.Prefs.Set("productId", "0x"+port.PID)
+			portJSON.Prefs.Set("serialNumber", port.SerialNumber)
+			portJSON.IdentificationPrefs.Set("pid", "0x"+port.PID)
+			portJSON.IdentificationPrefs.Set("vid", "0x"+port.VID)
+		}
+		portsJSON = append(portsJSON, portJSON)
+	}
+	d, err := json.MarshalIndent(&listOutputJSON{
+		EventType: "list",
+		Ports:     portsJSON,
+	}, "", "  ")
+	if err != nil {
+		outputError(err)
+		return
+	}
+	fmt.Println(string(d))
+}
+
+type messageOutputJSON struct {
+	EventType string `json:"eventType"`
+	Message   string `json:"message"`
+}
+
+func outputMessage(eventType, message string) {
+	d, err := json.MarshalIndent(&messageOutputJSON{
+		EventType: eventType,
+		Message:   message,
+	}, "", "  ")
+	if err != nil {
+		outputError(err)
+	} else {
+		fmt.Println(string(d))
+	}
+}
+
+func outputError(err error) {
+	outputMessage("error", err.Error())
+}
