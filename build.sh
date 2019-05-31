af3b49889fdee5d27cc03fa44276f5865c97db99
Cristian Maglie <c.maglie@arduino.cc>
2019-05-31 16:44:38 +0200

    Updated build.sh

--- build.sh
+++ build.sh
@@ -4,21 +4,24 @@
 
 rm -rf distrib
 
-# TODO: replace me with proper go modules stuff
-export GOPATH=$PWD
-
 export CGO_ENABLED=0
-GOOS=linux GOARCH=amd64 go build -o distrib/linux64/serial-discovery
-GOOS=linux GOARCH=386 go build -o distrib/linux32/serial-discovery
-GOOS=linux GOARCH=arm go build -o distrib/linuxarm/serial-discovery
-GOOS=linux GOARCH=arm64 go build -o distrib/linuxarm64/serial-discovery
-GOOS=windows GOARCH=386 GO386=387 go build -o distrib/windows/serial-discovery.exe
-CGO_ENABLED=1 CC=o64-clang GOOS=darwin GOARCH=amd64 go build -ldflags="-extldflags=-mmacosx-version-min=10.9" -o distrib/darwin/serial-discovery
+GOOS=linux GOARCH=amd64 go build -o distrib/linux64/bin/serial-discovery
+GOOS=linux GOARCH=386 go build -o distrib/linux32/bin/serial-discovery
+GOOS=linux GOARCH=arm go build -o distrib/linuxarm/bin/serial-discovery
+GOOS=linux GOARCH=arm64 go build -o distrib/linuxarm64/bin/serial-discovery
+GOOS=windows GOARCH=386 GO386=387 go build -o distrib/windows/bin/serial-discovery.exe
+CGO_ENABLED=1 CC=o64-clang GOOS=darwin GOARCH=amd64 go build -ldflags="-extldflags=-mmacosx-version-min=10.9" -o distrib/darwin/bin/serial-discovery
 
+VERSION=`git describe --tag`
 cd distrib
-zip -r ../serial-discovery-${VERSION}.zip *
+cd windows && zip -r ../../serial-discovery-windows-$VERSION.zip * && cd ..
+cd osx && tar cjf ../../serial-discovery-macosx-$VERSION.tar.bz2 * && cd ..
+cd linuxarm && tar cjf ../../serial-discovery-linuxarm-$VERSION.tar.bz2 * && cd ..
+cd linuxarm64 && tar cjf ../../serial-discovery-linuxarm64-$VERSION.tar.bz2 * && cd ..
+cd linux32 && tar cjf ../../serial-discovery-linux32-$VERSION.tar.bz2 * && cd ..
+cd linux64 && tar cjf ../../serial-discovery-linux64-$VERSION.tar.bz2 * && cd ..
 cd ..
 
-shasum serial-discovery-${VERSION}.zip
-sha256sum serial-discovery-${VERSION}.zip
-ls -la serial-discovery-${VERSION}.zip
+shasum serial-discovery*-${VERSION}.*
+sha256sum serial-discovery*-${VERSION}.*
+ls -la serial-discovery*-${VERSION}.*
