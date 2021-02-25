44574493ccad2211875e8a1598921b92ec71a33a
Cristian Maglie <c.maglie@arduino.cc>
2021-02-25 17:19:53 +0100

    Add -v/--version command flag

---
+++ args.go
@@ -0,0 +1,41 @@
+//
+// This file is part of serial-discovery.
+//
+// Copyright 2021 ARDUINO SA (http://www.arduino.cc/)
+//
+// This software is released under the GNU General Public License version 3,
+// which covers the main part of arduino-cli.
+// The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to modify or
+// otherwise use the software for commercial activities involving the Arduino
+// software without disclosing the source code of your own applications. To purchase
+// a commercial license, send an email to license@arduino.cc.
+//
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var args struct {
+	showVersion bool
+}
+
+func parseArgs() {
+	for _, arg := range os.Args[1:] {
+		if arg == "" {
+			continue
+		}
+		if arg == "-v" || arg == "--version" {
+			args.showVersion = true
+			continue
+		}
+		fmt.Fprintf(os.Stderr, "invalid argument: %s\n", arg)
+		os.Exit(1)
+	}
+}
